2cd07f7016ceb7204e1328f1d4d97e14519a8d33
Dan <dfernandez226@gmail.com>
2015-09-28 11:23:44 -0400

    Comments
    Commented everything

--- MemberLister.py
+++ MemberLister.py
@@ -1,9 +1,13 @@
 ﻿import openpyxl
 from openpyxl import *
 
+#Load your excel spreadsheet
 wb = load_workbook('UpdatedList.xlsx')
+
+#Select the worksheet, in this case the only one.
 ws = wb.active
 
+#very non-pythonic way of grouping the letters per the YaYas directory
 FirstFile = ['A', 'B', 'C']
 SecondFile = ['D', 'E', 'F']
 ThirdFile = ['G', 'H', 'I', 'J'] 
@@ -12,13 +16,14 @@ FifthFile = ['O', 'P', 'Q', 'R', 'S']
 SixthFile = ['T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 files = [FirstFile, SecondFile, ThirdFile, FourthFile, FifthFile, SixthFile]
 
+#clear out each output file
 for file in files:
-    text_file = open(str(file[0] + '-' + file[-1] +'.txt'), "w")
+    text_file = open(str(file[0] + '-' + file[-1] +'.txt'), "w") #files are named "X-Y.txt" where X is the first letter of the range and Y is the last
     text_file.write('')
     text_file.close()
 
 def GetLastName(num):
-    return ws['A' + str(num)].value.rstrip("()0123456789) ")
+    return ws['A' + str(num)].value.rstrip("()0123456789) ") #need to strip some extra info from last name column
 
 def GetFirstName(num):
     return ws['B' + str(num)].value
@@ -58,6 +63,11 @@ for row in range(2, 144):
     currentEmail = GetEmail(row)
     currentYear = str(GetMemberYear(row))
 
+
+    '''
+    If the cell is empty, NoneType is returned. We need to make these NoneTypes 
+    into blank strings so they can be put together with the html tags.
+    '''
     if currentHomeNumber is None:
         currentPhone = currentCell
         currentHomeNumber = ""
@@ -72,6 +82,7 @@ for row in range(2, 144):
     if currentAddress1 is None:
         currentAddress1 = ""
 
+    #very non-pythonic way of selecting our file to open based on last name; aka alphabetically sorting
     if GetLastName(row)[0] in FirstFile:
         fileToOpen = "A-C.txt"
 
@@ -93,7 +104,7 @@ for row in range(2, 144):
     text_file = open(fileToOpen, "a")
 
 
-    if ws['A' + str(row)].font.color.rgb == 'FFFF00FF':
+    if ws['A' + str(row)].font.color.rgb == 'FFFF00FF': #If name is in pink on spreadsheet, they are a special case and need additioanl HTML
         text_file.write( '<tr>\n' +
                 '    <td width="138">\n' +
                 '        <p style="margin-right: 10px; margin-top: 5px; margin-bottom: 5px">\n' +
@@ -110,7 +121,7 @@ for row in range(2, 144):
                 '            </font>\n'+
                 '    </td>\n' +
 	            '</tr>\n')
-    else:
+    else: #non-pink names for the standard html
         text_file.write( '<tr>\n' +
                 '    <td width="138">\n' +
                 '        <p style="margin-right: 10px; margin-top: 5px; margin-bottom: 5px">\n' +
@@ -127,4 +138,6 @@ for row in range(2, 144):
                 '            </font>\n'+
                 '    </td>\n' +
 	            '</tr>\n')
-    text_file.close()
+
+    text_file.close()
+
